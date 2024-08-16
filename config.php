9e2424d4b8180bf6ff480b7ebf655bc4d8222cd6
chalantorn2 <chalantorn2@gmail.com>
2024-08-16 10:46:22 +0700

    Fix config

--- config.php
+++ config.php
@@ -5,8 +5,9 @@ $password = getenv('DATABASE_PASSWORD');
 $dbname = getenv('DATABASE_NAME');
 
 try {
-    $conn = new PDO("pgsql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
+    // เพิ่ม sslmode=require เพื่อบังคับใช้ SSL ในการเชื่อมต่อ
+    $conn = new PDO("pgsql:host=$servername;dbname=$dbname;sslmode=require", $username, $password);
+    // ตั้งค่าให้ PDO แสดงข้อผิดพลาด
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
